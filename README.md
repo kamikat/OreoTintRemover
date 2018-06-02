1e46cc01d618a0a3842280c8e23404cab5f7ce80
kamikat <kamikat@banana.moe>
2018-06-02 08:11:19 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# OreoTintRemover
+
+One of the most foolish things I found immediately after upgrading from Nougat to Oreo (Android 8.1) is that
+they paint my wallpaper like a sh*t! Surprise, that's the new [feature][1] of cool Android!
+
+Well, let's fix the "feature":
+
+1. Get rooted
+2. Install [Xposed Framework][2]
+3. Install [OreoTintRemover][3] (signed by debug key)
+4. Enable the module in Xposed Installer
+5. Reboot
+
+
+
+## Build
+
+```
+./gradlew assembleDebug
+```
+
+## License
+
+(The WTFPL)
+
+[1]: https://www.androidpolice.com/2017/10/26/android-8-1-feature-spotlight-wallpaper-based-themes-include-tinted-notification-power-menu-shadows-just-like-pixel-2/
+[2]: http://dl-xda.xposed.info/framework/sdk27/
+[3]: https://github.com/kamikat/OreoTintRemover/releases
---
+++ app/src/main/java/moe/banana/mods/notint/Monkey.java
@@ -0,0 +1,44 @@
+package moe.banana.mods.notint;
+
+import java.lang.reflect.Field;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public class Monkey implements IXposedHookLoadPackage {
+
+    private void hijack(ClassLoader classLoader, final String className, String methodName, final String varName, final Object value) {
+        findAndHookMethod(className, classLoader, methodName, new XC_MethodHook() {
+
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Object obj = param.thisObject;
+                XposedBridge.log("Re-write " + className + "." + varName + "...");
+                Field field = obj.getClass().getDeclaredField(varName);
+                field.setAccessible(true);
+                field.set(obj, value);
+                XposedBridge.log("Re-write " + className + "." + varName + "... success.");
+            }
+        });
+    }
+
+    private void hijackAll(ClassLoader classLoader, String className) {
+        hijack(classLoader, className, "getMainColor", "mMainColor", 0xff212121);
+        hijack(classLoader, className, "getSecondaryColor", "mSecondaryColor", 0xff000000);
+        hijack(classLoader, className, "supportsDarkText", "mSupportsDarkText", false);
+    }
+
+    @Override
+    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+        if (!lpparam.packageName.equals("com.android.systemui"))
+            return;
+        XposedBridge.log("Caught systemui!");
+        hijackAll(lpparam.classLoader, "com.android.internal.colorextraction.ColorExtractor$GradientColors");
+        XposedBridge.log("Hooks injected.");
+    }
+}
